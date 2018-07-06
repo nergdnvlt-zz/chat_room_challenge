065c6536db0990dc51c8f39082aad979348fedea
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-06 11:31:26 -0600

    Not completed.

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,6 +1,11 @@
 class ChatRoomsController < ApplicationController
+  def index
+    @user = current_user
+    @chats = @user.chat_rooms
+  end
   def show
     @chat = ChatRoom.includes(:messages).find(params[:id])
     @chat_messages = @chat.messages.order(created_at: 'ASC')
+    @message = Message.new()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :chat_rooms, only: [:show]
+  resources :chat_rooms, only: [:index, :show]
+
+  root 'welcome#index'
+
+  get '/login', to: 'welcome#show', as: 'login'
+  post '/login', to: 'welcome#create'
 
   mount ActionCable.server => '/cable'
 end
